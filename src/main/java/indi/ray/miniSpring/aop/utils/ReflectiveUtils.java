7aa3674bfaf2923d50a23a707ec5f842f3577d8f
ray wang <wangrui@bongmi.com>
2018-11-26 12:01:21 +0800

    feat: find all interfaces of a class

--- src/main/java/indi/ray/miniSpring/aop/utils/ReflectiveUtils.java
+++ src/main/java/indi/ray/miniSpring/aop/utils/ReflectiveUtils.java
@@ -1,8 +1,11 @@
 package indi.ray.miniSpring.aop.utils;
 
+import indi.ray.miniSpring.core.utils.AssertUtils;
 import org.apache.log4j.Logger;
 
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReflectiveUtils {
     private static final Logger logger = Logger.getLogger(ReflectiveUtils.class);
@@ -29,5 +32,36 @@ public class ReflectiveUtils {
                     + methodName + "in its superclasses");
         }
         return method;
+    }
+
+    public static Class<?>[] getAllInterfacesForClass(Class<?> clz) {
+        AssertUtils.assertNotNull(clz, "class should not be null");
+        Set<Class<?>> interfaces = getAllInterfacesForClassAsSet(clz);
+        return interfaces.toArray(new Class<?>[interfaces.size()]);
+    }
+
+    public static Set<Class<?>> getAllInterfacesForClassAsSet(Class<?> clz) {
+        Set<Class<?>> interfaceSet = new HashSet<Class<?>>();
+
+        boolean shouldPropagateToSuper = true;
+        if (clz.isInterface()) {
+            shouldPropagateToSuper = false;
+            interfaceSet.add(clz);
+        }
+
+        if (shouldPropagateToSuper) {
+            Class<?> clazz = clz;
+            while (clazz != null) {
+                for (Class<?> intClz : clazz.getInterfaces()) {
+                    interfaceSet.addAll(getAllInterfacesForClassAsSet(intClz));
+                }
+                clazz = clazz.getSuperclass();
+            }
+        } else {
+            for (Class<?> intClz : clz.getInterfaces()) {
+                interfaceSet.addAll(getAllInterfacesForClassAsSet(intClz));
+            }
+        }
+        return interfaceSet;
     }
 }
